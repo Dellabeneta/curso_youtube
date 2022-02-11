c75ae8b12bb48f934e5c783c7816276a05d09911
Michel Dellabeneta <michel.dellabeneta@outlook.com>
2022-02-11 00:05:27 -0300

    .

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ terraform {
   }
 }
 
-#inserindo chave pública no servidor para acesso posterior.
+#inserindo chave pública no servidor para acesso remoto sem login/senha.
 resource "digitalocean_ssh_key" "curso" {
   name       = "curso"
   public_key = file("C:/Users/Administrador/.ssh/id_rsa.pub")
